f58aa67735b4fa59cd867e3052396ae23222156f
Allen Zhong <zhong.allen@gmail.com>
2016-08-24 23:14:12 +1200

    re-organized code

--- YAWA/YAWAWeather.h
+++ YAWA/YAWAWeather.h
@@ -1,12 +1,15 @@
 //
-//  WeaherCondition.m
+//  WeatherCondition.h
 //  YAWA
 //
 //  Created by 黄健 on 16/8/24.
 //  Copyright (c) 2016年 Jian Huang. All rights reserved.
 //
-
-#import <Foundation/Foundation.h>
 #import "YAWAWeather.h"
 
+@implementation YAWAWeather
+
+
 @end
+
+
---
+++ YAWA/YAWAWeather.m
@@ -0,0 +1,30 @@
+//
+//  WeaherCondition.m
+//  YAWA
+//
+//  Created by 黄健 on 16/8/24.
+//  Copyright (c) 2016年 Jian Huang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface YAWAWeather : NSObject<NSObject>
+
+@property (nonatomic, strong) NSDate *date;
+@property (nonatomic, strong) NSNumber *humidity;
+@property (nonatomic, strong) NSNumber *temperature;
+@property (nonatomic, strong) NSNumber *tempHigh;
+@property (nonatomic, strong) NSNumber *tempLow;
+@property (nonatomic, strong) NSString *locationName;
+@property (nonatomic, strong) NSDate *sunrise;
+@property (nonatomic, strong) NSDate *sunset;
+@property (nonatomic, strong) NSString *conditionDescription;
+@property (nonatomic, strong) NSString *condition;
+@property (nonatomic, strong) NSNumber *windBearing;
+@property (nonatomic, strong) NSNumber *windSpeed;
+@property (nonatomic, strong) NSString *icon;
+
+- (NSString *)imageName;
+
+@end
